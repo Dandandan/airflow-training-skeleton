637528c566be110d6b20609117163959846b4d96
Daniël Heres <dheres@bol.com>
2019-02-25 13:40:53 +0100

    Do some branching

--- dags/dag.py
+++ dags/dag.py
@@ -18,11 +18,9 @@ print_execution_date = BashOperator(
     task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
 )
 
-
 wait_1 = BashOperator(
     task_id="wait_1", bash_command="wait 1", dag=dag
 )
-
 
 wait_5 = BashOperator(
     task_id="wait_5", bash_command="wait 5", dag=dag
@@ -32,20 +30,19 @@ wait_10 = BashOperator(
     task_id="wait_10", bash_command="wait 10", dag=dag
 )
 
-
-
 options = ['branch a', 'branch b', 'branch c', 'branch d']
 
 
 def random_branch():
     random.choice(options)
 
+
 branching = BranchPythonOperator(
-    task_id= 'branching',
+    task_id='branching',
     python_callable=random_branch,
     dag=dag)
 
 end = DummyOperator(task_id="dummy", dag=dag)
 
 for option in options:
-    print_execution_date >> [ wait_1, wait_5, wait_10] >> branching >> option >> end
+    print_execution_date >> [wait_1, wait_5, wait_10] >> branching >> DummyOperator(task_id=option, dag=dag) >> end
