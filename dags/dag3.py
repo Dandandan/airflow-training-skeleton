e06982d4396a3ca9722fd3bf73ab8e136595559c
Daniël Heres <dheres@bol.com>
2019-02-26 10:51:33 +0100

    Test new http op

--- dags/dag3.py
+++ dags/dag3.py
@@ -2,6 +2,46 @@ from airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStor
 import airflow
 from airflow import DAG
 
+from tempfile import NamedTemporaryFile
+
+from airflow.hooks.http_hook import HttpHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
+
+
+class HttpToGcsOperator(BaseOperator):
+    """
+
+
+    """
+
+    template_fields = ()
+    template_ext = ()
+
+    ui_color = "#f4a460"
+
+    @apply_defaults
+    def __init__(self, url, bucket, *args, **kwargs):
+        self.url = url
+        self.bucket = bucket
+
+        super(HttpToGcsOperator, self).__init__()
+
+    def execute(self, context):
+        named_file = NamedTemporaryFile()
+        http = HttpHook(method="GET")
+        res = http.run(self.url)
+
+        named_file.write(res.json())
+
+        gcs = GoogleCloudStorageHook()
+
+        gcs.upload(self.bucket, "abc.json", named_file.name)
+
+        named_file.close()
+
+
 dag = DAG(
     dag_id='real_estate',
     default_args={
@@ -10,10 +50,15 @@ dag = DAG(
     }
 )
 
-pgsql_to_gcs = PostgresToGoogleCloudStorageOperator(
-    task_id="postgres_job",
+http_op = HttpToGcsOperator(
+    url="https://us-central1-umcg-cardio-ai.cloudfunctions.net/convert-currency?date=2018-01-01&from=GBP&to=EUR",
     bucket="airflow-daniel",
-    sql="select * from land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
-    filename="land_registry_price_paid_uk/{{ ds }}/land_registry_price.json",
-    dag=dag
-)
+    dag=dag)
+
+# pgsql_to_gcs = PostgresToGoogleCloudStorageOperator(
+#    task_id="postgres_job",
+#    bucket="airflow-daniel",
+#    sql="select * from land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
+#    filename="land_registry_price_paid_uk/{{ ds }}/land_registry_price.json",
+#    dag=dag
+# )
