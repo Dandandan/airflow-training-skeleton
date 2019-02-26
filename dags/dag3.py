ff46146960717de53c3119e09065b2ac762eb4b5
Daniël Heres <dheres@bol.com>
2019-02-26 10:07:39 +0100

    Add new dag

---
+++ dags/dag3.py
@@ -0,0 +1,19 @@
+from airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
+import airflow
+from airflow import DAG
+
+dag = DAG(
+    dag_id='real_estate',
+    default_args={
+        'owner': 'daniel',
+        'start_date': airflow.utils.dates.days_ago(2)
+    }
+)
+
+pgsql_to_gcs = PostgresToGoogleCloudStorageOperator(
+    task_id="postgres_job",
+    bucket="gs://airflow-dheres",
+    sql="select * from land_registry_price_paid_uk",
+    filename="xxx.json",
+    dag=dag
+)
