98eb699b2004d3451293f2cc7a37fc80e0d73817
Daniël Heres <dheres@bol.com>
2019-02-26 10:57:20 +0100

    Test new http op

--- dags/dag3.py
+++ dags/dag3.py
@@ -51,7 +51,7 @@ dag = DAG(
 )
 
 http_op = HttpToGcsOperator(
-    url="https://us-central1-umcg-cardio-ai.cloudfunctions.net/convert-currency?date=2018-01-01&from=GBP&to=EUR",
+    url="convert-currency?date={{ ds }}&from=GBP&to=EUR",
     bucket="airflow-daniel",
     task_id="conversion_rate",
     dag=dag)
